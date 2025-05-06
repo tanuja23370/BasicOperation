8e4043523498e9ec4aee01669fc1487a31b2a2c8
tanuja23370 <tanujanimbakkanavar23@gmail.com>
2025-05-06 10:06:36 -0300

    Save

---
+++ DivisionOperation.java
@@ -0,0 +1,13 @@
+package Calculator;
+public class DivisionOperation {
+    public static void main(String[] args) {
+        int a=15;
+        int b=0;
+        try{
+            int div=a/b;
+            System.out.println("Division is: "+div);
+        } catch(ArithmeticException e){
+            System.out.println("Error:Cannot divide by zero");
+    }
+}
+}
